c1709f51be35a9d6c3bdf8e653557ee97a8808bb
Janick Bunzel <janick@bunzel-online.de>
2025-06-04 23:27:21 +0200

    correct Subject typing

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -20,6 +20,20 @@ export type NoteString =
 
 export type NoteNumber = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 | 13 | 14 | 15;
 
+export const allSubjects = [
+    'mathe',
+    'deutsch',
+    'englisch',
+    'biologie',
+    'chemie',
+    'physik',
+    'wuk',
+    'kunst',
+    'sport',
+    'musik',
+];
+export type Subject = (typeof allSubjects)[number];
+
 export type Grade = {
     subject: Subject;
     level?: Level;
@@ -28,18 +42,6 @@ export type Grade = {
     zap?: NoteString;
     projekt?: NoteString;
 };
-
-export type Subject =
-    | 'mathe'
-    | 'deutsch'
-    | 'englisch'
-    | 'biologie'
-    | 'chemie'
-    | 'physik'
-    | 'wuk'
-    | 'kunst'
-    | 'sport'
-    | 'musik';
 
 export type GradeSubmission = Grade[];
 
